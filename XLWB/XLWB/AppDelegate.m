bf01d2be55e7c0e849fc71ff14d18811badf093e
zb <zb@163.com>
2016-05-03 19:46:05 +0800

    自己的事情自己做，j将AppDelegate类中的代码剪切到ZBTabBarController中

--- XLWB/XLWB/AppDelegate.m
+++ XLWB/XLWB/AppDelegate.m
@@ -7,11 +7,8 @@
 //
 
 #import "AppDelegate.h"
-#import "ZBHomeViewController.h"
-#import "ZBDiscoverViewController.h"
-#import "ZBMessageCenterController.h"
-#import "ZBProfileViewController.h"
 
+#import "ZBTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -26,80 +23,16 @@
     self.window.frame = [UIScreen mainScreen].bounds;
     
     // 2.设置根控制器
-    UITabBarController *tabbarVC = [[UITabBarController alloc] init];
+    ZBTabBarController *tabbarVC = [[ZBTabBarController alloc] init];
     self.window.rootViewController = tabbarVC;
     
-    // 3.设置子控制器
- //   UIViewController *vc1 = [[UIViewController alloc] init];
-//vc1.view.backgroundColor = ZBRandomColor;
-//    vc1.tabBarItem.title = @"首页";
-//    vc1.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
-//   vc1.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_home_selected"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    
-    ZBHomeViewController *home = [[ZBHomeViewController alloc] init];
-    [self addChildVc:home title:@"首页" image:@"tabbar_home" selectedImage:@"tabbar_home_selected" bgColor:ZBRandomColor];
-    
-//    UIViewController *vc2 = [[UIViewController alloc] init];
-//    vc2.view.backgroundColor = ZBRandomColor;
-//    vc2.tabBarItem.title = @"消息";
-//    vc2.tabBarItem.image = [UIImage imageNamed:@"tabbar_message_center"];
-//    vc2.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_message_center_selected"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    
-    ZBMessageCenterController *messageCenter = [[ZBMessageCenterController alloc] init];
-    [self addChildVc:messageCenter title:@"消息" image:@"tabbar_message_center" selectedImage:@"tabbar_message_center_selected" bgColor:ZBRandomColor];
-    
-    
-//    UIViewController *vc3 = [[UIViewController alloc] init];
-//    vc3.view.backgroundColor = ZBRandomColor;
-//    vc3.tabBarItem.title = @"发现";
-//    vc3.tabBarItem.image = [UIImage imageNamed:@"tabbar_discover"];
-//    vc3.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_discover_selected"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    ZBDiscoverViewController *discover = [[ZBDiscoverViewController alloc] init];
-    [self addChildVc:discover title:@"发现" image:@"tabbar_discover" selectedImage:@"tabbar_discover_selected" bgColor:ZBRandomColor];
-    
-//    UIViewController *vc4 = [[UIViewController alloc] init];
-//    vc4.view.backgroundColor = ZBRandomColor;
-//    vc4.tabBarItem.title = @"我";
-//    vc4.tabBarItem.image = [UIImage imageNamed:@"tabbar_profile"];
-//    vc4.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_profile_selected"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    ZBProfileViewController *profile = [[ZBProfileViewController alloc] init];
-    [self addChildVc:profile title:@"我" image:@"tabbar_profile" selectedImage:@"tabbar_profile_selected" bgColor:ZBRandomColor];
-    
-    
-    //等价    tabbarVC.viewControllers =@[vc1,vc2,vc3,vc4];
-    [tabbarVC addChildViewController:home];
-    [tabbarVC addChildViewController:messageCenter];
-    [tabbarVC addChildViewController:discover];
-    [tabbarVC addChildViewController:profile];
-
+  
     
     
     // 4.显示窗口
     [self.window makeKeyAndVisible];
     
     return YES;
-
-}
--(void)addChildVc:(UIViewController *)childVc title:(NSString *)title image:(NSString *)image selectedImage: (NSString *)selectedImage bgColor:(UIColor *)bgColor{
-    // 设置子控制器的文字和图片
-    childVc.tabBarItem.title = title;
-    childVc.tabBarItem.image = [UIImage imageNamed:image];
-    childVc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    
-    // 设置文字的样式
-    NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
-    textAttrs[NSForegroundColorAttributeName] = ZBColor(123, 123, 123);
-    
-    NSMutableDictionary *selectTextAttrs = [NSMutableDictionary dictionary];
-    selectTextAttrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
-    
-    [childVc.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
-    [childVc.tabBarItem setTitleTextAttributes:selectTextAttrs forState:UIControlStateSelected];
-    
-    childVc.view.backgroundColor = bgColor;
-    
-    
-
 
 }
 
