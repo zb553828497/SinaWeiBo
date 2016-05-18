dad98e0f5250e205ac663d370d500a59ab56e2a3
zb <zb@163.com>
2016-05-18 11:46:35 +0800

    下拉刷新时,showNewStatusCount弹框提示最新微博数量

--- XLWB/XLWB/Classes/Home(首页)/Controller/ZBHomeViewController.m
+++ XLWB/XLWB/Classes/Home(首页)/Controller/ZBHomeViewController.m
@@ -41,8 +41,8 @@
         
     }
     return _statuses;
-
-
+    
+    
 }
 
 - (void)viewDidLoad {
@@ -56,17 +56,25 @@
     [self setupUserInfo];
     
     // 加载最新的微博数据
-  //  [self loadNewStatus];
+    //  [self loadNewStatus];
     // 集成刷新控件
     [self setupRefresh];
 }
-
+// 只会调用1次。因为setupRefresh是viewDidLoad中的方法，viewDidLoad方法只会调用1次，所以他里面的方法也只会调用一次
 -(void)setupRefresh{
+    // 1.添加刷新控件
     UIRefreshControl *Ref = [[UIRefreshControl alloc]init];
+    // 只有用户通过手动下拉刷新(注意)，才会触发UIControlEventValueChanged事件,否则不执行StateChange:
     [Ref addTarget:self action:@selector(StateChange:) forControlEvents:UIControlEventValueChanged];
     [self.tableView addSubview:Ref] ;
+    // 2.程序运行,自动进入刷新状态(仅仅是显示刷新状态,并不会触发UIControlEventValueChanged事件)
+    // 只会调用1次
+    [Ref beginRefreshing];
+    // 3.程序运行自动调用StateChange方法加载最新数据。
+    // 只会调用1次，以后不会再调用.因为本质上这些方法是在viewDidLoad中调用的。又因为整个过程只会执行一次viewDidLoad方法，所以StateChange:方法从始至终只会调用一次
+    [self StateChange:Ref];
     
-
+    
 }
 /**
  *  UIRefreshControl进入刷新状态：加载最新的数据
@@ -87,11 +95,11 @@
     // 取出最前面(时间最新，ID最大)的微博
     ZBStatus *firstStatus = [self.statuses firstObject];
     
-//判断firstStatus是否有数据,必须得判断,因为如果没有数据,firstStatus.idstr就为空,给since_id赋空值,程序被崩掉
-// 如果判断不满足，则向下执行，把responseObject中的数据显示在cell上。如果满足,则把最新的微博插入到最前面
+    //判断firstStatus是否有数据,必须得判断,因为如果没有数据,firstStatus.idstr就为空,给since_id赋空值,程序被崩掉
+    // 如果判断不满足，则向下执行，把responseObject中的数据显示在cell上。如果满足,则把最新的微博插入到最前面
     if (firstStatus != nil) {
- // 若指定此参数(since_id)，则只返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0.
-// 因为上面的代码中,firstStatus是ID最大的微博，又把firstStatus.idstr赋值给了since_id，又因为指定了since_id，则只返回ID比since_id大的微博，所以就保证了不会把之前显示的微博内容再次返回给用户(之前显示的微博内容对应的ID肯定比firstStatus小，因为最前面的微博,ID就是最大的)。这个就是since_id的作用，我们不需要问为什么,底层开发人员就是这么设计的
+        // 若指定此参数(since_id)，则只返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0.
+        // 因为上面的代码中,firstStatus是ID最大的微博，又把firstStatus.idstr赋值给了since_id，又因为指定了since_id，则只返回ID比since_id大的微博，所以就保证了不会把之前显示的微博内容再次返回给用户(之前显示的微博内容对应的ID肯定比firstStatus小，因为最前面的微博,ID就是最大的)。这个就是since_id的作用，我们不需要问为什么,底层开发人员就是这么设计的
         params[@"since_id"] = firstStatus.idstr;
     }
     
@@ -114,12 +122,63 @@
         
         // 结束刷新
         [control endRefreshing];
+        
+        // 显示最新微博的数量
+        [self showNewStatusCount:newStatuses.count];
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         [control endRefreshing];
     }];
-
+    
 }
 
+-(void)showNewStatusCount:(int)count{
+    
+    // 1.创建label
+    UILabel *label = [[UILabel alloc] init];
+    label.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"timeline_new_status_background"]];
+    label.zb_width = [UIScreen mainScreen].bounds.size.width;
+    label.zb_height = 35;
+    
+    // 2.设置其他属性
+    if(count == 0){
+    label.text = @"没有新的数据,请稍后再试";
+    }else{
+        label.text = [NSString stringWithFormat:@"共有%d条新的微博数据",count];
+    }
+    label.textColor = [UIColor whiteColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont systemFontOfSize:16];
+    
+    // 3.添加
+    label.zb_Y = 64 - label.zb_height;
+    //[self.tableView addSubview:label]; // label会跟随tableview滚动,不满足要求
+    //[self.navigationController.view addSubview:label];// label会挡住航控制器的view,不满足要求
+    
+    // 将label插入到导航栏的下面(below)，tableView的上面
+    [self.navigationController.view insertSubview:label belowSubview:self.navigationController.navigationBar];
+    
+    // 4.动画
+    
+    // 先利用1s的时间,让label往下移动一段距离
+    CGFloat duration = 1.0;
+    [UIView animateWithDuration:duration animations:^{
+        
+        //等价  label.zb_Y += label.zb_height;
+        label.transform = CGAffineTransformMakeTranslation(0, label.zb_height);
+    } completion:^(BOOL finished) {
+        // 延迟1s后，再利用1s的时间，让label往上移动一段距离（回到一开始的状态）
+        CGFloat delay = 1.0;
+        // UIViewAnimationOptionCurveLinear:匀速
+        [UIView animateWithDuration:duration delay:delay options:UIViewAnimationOptionCurveLinear animations:^{
+            // 等价  label.zb_Y -=label.zb_height;
+            label.transform = CGAffineTransformIdentity;
+        } completion:^(BOOL finished) {
+            [label removeFromSuperview];
+        }];
+    }];
+      // 总结:如果某个动画执行完毕后，又要回到动画执行前的状态，建议使用transform来做动画
+    
+}
 -(void)loadNewStatus{
     /*
      URL:
@@ -142,20 +201,20 @@
     
     // 3.发送请求
     [mgr GET:@"https://api.weibo.com/2/statuses/friends_timeline.json" parameters:params success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-       // NSLog(@"请求成功-%@",responseObject);
-       
+        // NSLog(@"请求成功-%@",responseObject);
+        
         // 取得 “微博字典”数组
-    // statuses 是服务器返回的responseObject(响应体)中的key，这个key可不能随便写，否则找不到value哦
-    // responseObject[@"statuses"]的整体含义:根据key得到的value
-
+        // statuses 是服务器返回的responseObject(响应体)中的key，这个key可不能随便写，否则找不到value哦
+        // responseObject[@"statuses"]的整体含义:根据key得到的value
+        
         NSArray *dictArray = responseObject[@"statuses"];
         
         // 将字典数组转为模型数组
         for (NSDictionary *dict in dictArray) {
             ZBStatus *status = [ZBStatus objectWithKeyValues:dict];
             [self.statuses addObject:status];
-                  }
-       // 刷新表格
+        }
+        // 刷新表格
         [self.tableView reloadData];
         
         
@@ -182,10 +241,10 @@
     // 1.请求管理者
     AFHTTPSessionManager * mgr = [AFHTTPSessionManager manager];
     
-   
+    
     // account方法返回值类型为ZBAccount，所以=左边用ZBAccount类型的对象接收
     ZBAccount *account = [ZBAccountTool account];
-   
+    
     // 2.拼接请求参数
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
     // account.access_token 存储着用户的access_token。
@@ -196,7 +255,7 @@
     
     // 3.发送请求
     [mgr GET:@"https://api.weibo.com/2/users/show.json" parameters:params success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-       // NSLog(@"请求成功%@",responseObject);
+        // NSLog(@"请求成功%@",responseObject);
         
         NSString *name = responseObject[@"name"];
         // 将导航栏中间的titleView强转成按钮，这样就能修改按钮中的文字还有图片了
@@ -221,12 +280,12 @@
     // 必须有self作为参数，这样ZBItemTool类才能拿到self，才能在ZBItemTool类中将self作为addTarget的参数，如果没有self作为参数，ZBItemTool类直接使用self，这个self就是ZBItemTool类的对象
     self.navigationItem.leftBarButtonItem = [UIBarButtonItem ItemWithTarget:self action:@selector(friendsearch) image:@"navigationbar_friendsearch" HighlightImage:@"navigationbar_friendsearch_highlighted"];
     self.navigationItem.rightBarButtonItem = [UIBarButtonItem ItemWithTarget:self action:@selector(pop) image:@"navigationbar_pop" HighlightImage:@"navigationbar_pop_highlighted"];
-   
+    
     
     /** 中间的标题按钮*/
     ZBTitleButton *titleButton = [[ZBTitleButton alloc] init];
-//    titleButton.zb_width = 150;
-//    titleButton.zb_height = 30;
+    //    titleButton.zb_width = 150;
+    //    titleButton.zb_height = 30;
     
     // [ZBAccountTool account]的作用:从沙盒中解档用户信息，得到的返回值是account.account里面有用户信息
     // [ZBAccountTool account].name 其实就是account.name，作用:从用户信息中取出用户的昵称
@@ -242,7 +301,7 @@
     // 如果图片的某个方向上不规则，比如有突起，那么这个方向最好不要拉伸,否则凸起可能消失，或者凸起会移动位置
     // 原因: 保护凸起部位，拉伸图片之后,凸起部位会移动位置。拉伸凸起部位，拉伸图片之后凸起部位会消失
     self.titleButton = titleButton;
-
+    
     /*
      //2 设置图片和文字的内边距，最终的效果:图片距离左边越来越远，文字距离右边原来越远，所以最终效果,文字在左,图片在右
      // 2.1 图片的内边距，70是距离左边的间距，值越大，距离左边的距离越远，最终图片向右移动
@@ -251,7 +310,7 @@
      titleButton.titleEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 40);
      
      */
-
+    
 }
 
 
@@ -272,7 +331,7 @@
     
     // 3.显示
     [menu Show:titleButton];
-
+    
 }
 /**
  *  下拉菜单被销毁了
@@ -284,20 +343,20 @@
 }
 
 -(void)DropDownMenuDidShow:(ZBDropDownMenu *)menu{
-NSLog(@"箭头朝上");
+    NSLog(@"箭头朝上");
     // 箭头朝上
     [self.titleButton setImage:[UIImage imageNamed:@"navigationbar_arrow_up"] forState:UIControlStateNormal];
-
+    
 }
 
 
 
 -(void)friendsearch{
-
+    
 }
 -(void)pop{
-
-
+    
+    
 }
 
 
@@ -317,7 +376,7 @@ NSLog(@"箭头朝上");
     ZBStatus *status = self.statuses[indexPath.row];
     
     // 取出这条微博的作者
-   ZBUser *user =  status.user;
+    ZBUser *user =  status.user;
     cell.textLabel.text = user.name;
     
     // 设置微博的文字
