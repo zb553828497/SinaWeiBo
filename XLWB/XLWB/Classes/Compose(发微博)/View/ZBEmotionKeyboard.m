d7a79fb9f52f5c3881db5000b9ccd922669d9281
zb <zb@163.com>
2016-06-01 16:24:55 +0800

    在ZBEmotionListView类中实现表情的分页效果

--- XLWB/XLWB/Classes/Compose(发微博)/View/ZBEmotionKeyboard.m
+++ XLWB/XLWB/Classes/Compose(发微博)/View/ZBEmotionKeyboard.m
@@ -14,8 +14,8 @@
 @interface ZBEmotionKeyboard()<ZBEmotionTabBarDelegate>
 
 
-/** 容纳表情内容的控件*/
-@property(nonatomic,weak)UIView *ContainsEmotion;
+/** 保存正在显示的listView*/
+@property(nonatomic,weak)ZBEmotionListView *SaveShowingListView;
 
 /** "最近"表情*/
 @property(nonatomic,strong)ZBEmotionListView *RecentListView;
@@ -35,41 +35,45 @@
 
 -(ZBEmotionListView *)RecentListView{
     if (_RecentListView == nil) {
+        // init会调用ZBEmotionListView中的initWithFrame方法
         self.RecentListView = [[ZBEmotionListView alloc] init];
-        self.RecentListView.backgroundColor = ZBRandomColor;
+        // 虽然没有设置_RecentListView界面的颜色，但是在ZBEmotionListView中的initWithFrame方法中已经设置了默认的红色
     }
     return _RecentListView;
 }
 
 -(ZBEmotionListView *)DefaultListView{
     if (_DefaultListView == nil) {
+        // init会调用ZBEmotionListView中的initWithFrame方法
         self.DefaultListView = [[ZBEmotionListView alloc] init];
         NSString *path = [[NSBundle mainBundle] pathForResource:@"EmotionIcons/default/info.plist" ofType:nil];
         // 字典数组转为ZBEmotion类型的模型数组，并将转成功之后模型数组中的"默认"表情存储到ZBEmotionListView类的emotions数组中
-        self.EmojiListView.emotions = [ZBEmotion mj_objectArrayWithKeyValuesArray:[NSArray arrayWithContentsOfFile:path]];
-        self.DefaultListView.backgroundColor = ZBRandomColor;
+        // 调用emotions属性的setter方法，在setter方法内部，设置pageControl的页数,以及创建用于显示表情的控件
+        self.DefaultListView.emotions = [ZBEmotion mj_objectArrayWithKeyValuesArray:[NSArray arrayWithContentsOfFile:path]];
     }
     return _DefaultListView;
 }
 
 -(ZBEmotionListView *)EmojiListView{
     if (_EmojiListView == nil) {
+        // init会调用ZBEmotionListView中的initWithFrame方法
         self.EmojiListView = [[ZBEmotionListView alloc] init];
         NSString *path = [[NSBundle mainBundle] pathForResource:@"EmotionIcons/emoji/info.plist" ofType:nil];
         // 字典数组转为ZBEmotion类型的模型数组，并将转成功之后模型数组中的"emoji"表情存储到ZBEmotionListView类的emotions数组中
+         // 调用emotions属性的setter方法，在setter方法内部，设置pageControl的页数,以及创建用于显示表情的控件
         self.EmojiListView.emotions = [ZBEmotion mj_objectArrayWithKeyValuesArray:[NSArray arrayWithContentsOfFile:path]];
-        self.EmojiListView.backgroundColor = ZBRandomColor;
     }
     return  _EmojiListView;
 }
 
 -(ZBEmotionListView *)LxhListView{
     if (_LxhListView == nil) {
+        // init会调用ZBEmotionListView中的initWithFrame方法
         self.LxhListView = [[ZBEmotionListView alloc] init];
         NSString *path = [[NSBundle mainBundle] pathForResource:@"EmotionIcons/lxh/info.plist" ofType:nil];
         // 字典数组转为ZBEmotion类型的模型数组，并将转成功之后模型数组中的"Lxh"表情存储到ZBEmotionListView类的emotions数组中
+         // 调用emotions属性的setter方法，在setter方法内部，设置pageControl的页数,以及创建用于显示表情的控件
         self.LxhListView.emotions = [ZBEmotion mj_objectArrayWithKeyValuesArray:[NSArray arrayWithContentsOfFile:path]];
-        self.LxhListView.backgroundColor = ZBRandomColor;
     }
     return _LxhListView;
 }
@@ -79,13 +83,7 @@
 -(instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
-        // 1.表情内容
-        UIView *ContainsEmotion = [[ZBEmotionListView alloc] init];
-      //  ContainsEmotion.backgroundColor = [UIColor redColor];
-        [self addSubview:ContainsEmotion];
-        self.ContainsEmotion = ContainsEmotion;
-        
-        // 2.tabbar
+        // tabbar
         ZBEmotionTabBar *tabBar = [[ZBEmotionTabBar alloc] init];
         //tabBar.backgroundColor = [UIColor greenColor];
         tabBar.delegate = self;
@@ -105,15 +103,12 @@
     
     
     // 2.表情内容
-    self.ContainsEmotion.zb_width = self.zb_width;
-    self.ContainsEmotion.zb_height = self.zb_height - self.tabBar.zb_height;
-    // x和y都为0，因为键盘没有父控件，系统默认从底往上弹出，弹出的高度就是你设定的高度。键盘停在哪里，哪里就为(0,0)
-    self.ContainsEmotion.zb_X = 0;
-    self.ContainsEmotion.zb_Y = 0;
+    self.SaveShowingListView.zb_width = self.zb_width;
+    self.SaveShowingListView.zb_height = self.zb_height - self.tabBar.zb_height;
     
-    // 3.设置frame(为什么必不可少？因为这个计算ContainsEmotion中的子控件的frame，上面的代码是计算ContainsEmotion自身的frame)
-    UIView *chileView = [self.ContainsEmotion.subviews lastObject];// lastObject firstObject都可以，因为ContainsEmotion中只有一个元素(因为每次点击键盘底部的tabbar就会移除ContainsEmotion中之前存储的控件，所以点击点击键盘底部的tabbar，只会有一个显示在ContainsEmotion上)。
-    chileView.frame = self.ContainsEmotion.bounds;
+    // x和y都为0，因为键盘没有父控件，系统默认从底往上弹出，弹出的高度就是你设定的高度。键盘停在哪里，哪里就为(0,0)
+    self.SaveShowingListView.zb_X = 0;
+    self.SaveShowingListView.zb_Y = 0;
     
 }
 
@@ -121,29 +116,56 @@
 // 点击键盘底部的tabbar就会调用
 -(void)emotionTabBar:(ZBEmotionTabBar *)tabBar didSelectButton:(ZBEmotionTabBarButtonType)buttonType{
     
-    // 移除ContainsEmotion上之前显示的控件，保证每次点击键盘底部的tabbar，只会把 最近/默认/Emoji/Lxh中的一个显示在表情键盘的ContainsEmotion中，不会存在覆盖的现象。
-    // 原理:只要删除ContainsEmotion，ContainsEmotion中的子控件也会被删除
-    [self.ContainsEmotion.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    // 把SaveShowingListView上之前显示的表情从父控件(当前类)移除，保证每次点击键盘底部的tabbar，只会把 最近/默认/Emoji/Lxh中的一个显示在表情键盘的SaveShowingListView中，不会存在覆盖的现象。
+    // 为什么SaveShowingListView的父控件是当前类。因为RecentListView，DefaultListView，EmojiListView，LxhListView都是添加到self，也就是当前类中的。而RecentListView又赋值给了SaveShowingListView，所以SaveShowingListView也相当于添加的哦啊了当前类中，所以SaveShowingListView的父控件就是当前类
+       [self.SaveShowingListView removeFromSuperview];
     
-    // 根据键盘底部tabbar中按钮的类型，切换ContainsEmotion上面的xxxxListView
-    switch (buttonType) {
-        case ZBEmotionTabBarButtonTypeRecent:// "最近"
-            [self.ContainsEmotion addSubview:self.RecentListView];
+    // 根据键盘底部tabbar中按钮的类型，切换对应的表情
+    switch (buttonType) { 
+        case ZBEmotionTabBarButtonTypeRecent:{// "最近"
+            // 调用 RecentListView的懒加载方法,将"最近"表情添加到当前类中
+            [self addSubview:self.RecentListView];
+            // 将"最近"表情赋值给SaveShowingListView，这样SaveShowingListView中就保存了"最近"表情。
+            /* 
+             1.为什么可以赋值？
+             答:因为两个对象的类型一致，所以SaveShowingListView等价RecentListView
+             
+             2.赋值之后的结果是什么？
+             答:RecentListView有啥，SaveShowingListView就有啥。RecentListView是经过alloc init创建出来的，那么SaveShowingListView也是经过alloc init创建出来的(虽然代码中没有使用alloc init创建出SaveShowingListView对象，但是因为赋值的关系，SaveShowingListView就是相当于利用alloc init创建出来的)。
+            
+             3.为什么要赋值给SaveShowingListView？
+            因为 RecentListView，DefaultListView，EmojiListView，LxhListView要显示出来，必须设置frame，我们把这四个对象都赋值给同一个对象SaveShowingListView，那么我们只需拿到SaveShowingListView，然后在layoutSubviews方法中设置4个表情的frame。
+             如果不赋值给同一个对象，那么我们在layoutSubviews方法中就得设置4个对象，来分别对这4个表情设置frame
+             */
+            self.SaveShowingListView = self.RecentListView;
             break;
-        case ZBEmotionTabBarButtonTypeDefault: // "默认"
-            [self.ContainsEmotion addSubview:self.DefaultListView];
+        }
+        case ZBEmotionTabBarButtonTypeDefault:{ // "默认"
+            // 调用 DefaultListView的懒加载方法,将"默认"表情添加到当前类中
+            [self addSubview:self.DefaultListView];
+            // 将"默认"表情赋值给SaveShowingListView，这样SaveShowingListView中就保存了"默认"表情。
+            // 为什么可以赋值？因为两个对象的类型一致，所以SaveShowingListView等价DefaultListView
+            self.SaveShowingListView = self.DefaultListView;
             break;
-        case ZBEmotionTabBarButtonTypeEmoji: // "emoji"
-            [self.ContainsEmotion addSubview:self.EmojiListView];
+        }
+        case ZBEmotionTabBarButtonTypeEmoji:{ // "emoji"
+            // 调用 EmojiListView的懒加载方法,将"emoji"表情添加到当前类中
+            [self addSubview:self.EmojiListView];
+            // 将"emoji"表情赋值给SaveShowingListView，这样SaveShowingListView中就保存了"emoji"表情。
+            // 为什么可以赋值？因为两个对象的类型一致，所以SaveShowingListView等价EmojiListView
+            self.SaveShowingListView = self.EmojiListView;
             break;
-        case ZBEmotionTabBarButtonTypeLxh: // "lxh"
-            [self.ContainsEmotion addSubview:self.LxhListView];
+        }
+        case ZBEmotionTabBarButtonTypeLxh:{ // "lxh"
+            // 调用 LxhListView的懒加载方法将"lxh"表情添加到当前类中
+            [self addSubview:self.LxhListView];
+            // 将"lxh"表情赋值给SaveShowingListView，这样SaveShowingListView中就保存了"最近"表情。
+            // 为什么可以赋值？因为两个对象的类型一致，所以SaveShowingListView等价LxhListView
+            self.SaveShowingListView = self.LxhListView;
             break;
+        }
     }
-    // 重新计算子控件的frame(setNeedsLayout内部会在恰当的时刻，重新调用layoutSubviews，重新布局子控件)
-    // 如果没有这句代码，ContainsEmotion上的子控件永远不会有frame，因为根本就没有执行layoutSubviews来设置子控件的frame。所以你即使设置了子控件的颜色，没有设置子控件的frame，那么颜色压根也不会显示。
-    // 技:子控件必须设置frame，子控件的颜色才知道在具体的哪个位置上显示。
-    [self setNeedsLayout];
-}
+    
+     }
 
 @end
--- XLWB/XLWB/Classes/Compose(发微博)/View/ZBEmotionListView.m
+++ XLWB/XLWB/Classes/Compose(发微博)/View/ZBEmotionListView.m
@@ -8,6 +8,105 @@
 
 #import "ZBEmotionListView.h"
 
+#define ZBEmotionEveryPageCount 20
+
+@interface ZBEmotionListView()<UIScrollViewDelegate>
+
+@property(nonatomic,weak)UIScrollView *scrollView;
+@property(nonatomic,weak)UIPageControl *pageControl;
+
+@end
+
 @implementation ZBEmotionListView
+// 初始化要显示的控件
+// ZBEmotionKeyboard类中的懒加载方法中对BEmotionListView类进行了alloc init操作,所以会来到initWithFrame:
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor whiteColor];
+        // 1.初始化UIScrollView控件
+        UIScrollView *scrollView = [[UIScrollView alloc] init];
+        scrollView.backgroundColor = [UIColor redColor];
+        scrollView.pagingEnabled = YES;
+        scrollView.delegate = self;
+        scrollView.showsHorizontalScrollIndicator = NO;
+        scrollView.showsHorizontalScrollIndicator = NO;
+        [self addSubview:scrollView];
+        self.scrollView = scrollView;
+        
+        // 2.初始化UIPageControl控件
+        UIPageControl *pageControl = [[UIPageControl alloc] init];
+        pageControl.userInteractionEnabled = YES;
+        // 设置pageControl中普通状态下的图片
+        // pageImage是系统的私有属性，利用kvc给私有属性赋值，从而没有选中的页就会变成自己设置的图片
+        [pageControl setValue:[UIImage imageNamed:@"compose_keyboard_dot_normal"] forKeyPath:@"pageImage"];
+        // 设置pageControl中选中状态下的图片
+         // currentPageImage是系统的私有属性，利用kvc给私有属性赋值，从而选中的页就会变成自己设置的图片
+        [pageControl setValue:[UIImage imageNamed:@"compose_keyboard_dot_selected"] forKeyPath:@"currentPageImage"];
+        [self addSubview:pageControl];
+        self.pageControl = pageControl;
+    }
+    return self;
+}
+
+// 根据传递过来的“默认”/"Emoji"/"Lxh"其中一类表情，计算这一类表情对一个的每一页表情个数
+// ZBEmotionKeyboard类中的懒加载方法中调用了emotions的setter方法
+-(void)setEmotions:(NSArray *)emotions{
+    
+    _emotions = emotions;
+    // 根据表情的个数计算出来的用几页UIView来存放这些表情
+    NSUInteger count = (emotions.count  + ZBEmotionEveryPageCount - 1) / ZBEmotionEveryPageCount;
+    // 1.设置页数
+    self.pageControl.numberOfPages = count;
+    // 2.创建用来显示每一页表情的控件
+    for(int i = 0; i < self.pageControl.numberOfPages; i++){
+        UIView *pageView = [[UIView alloc] init];
+        pageView.backgroundColor = ZBRandomColor;
+        // 为每一页设置一个UIView，并添加到scrollView上
+        [self.scrollView addSubview:pageView];
+    }
+}
+
+-(void)layoutSubviews{
+    
+    // 1.pageControl
+    self.pageControl.zb_width = self.zb_width;
+    self.pageControl.zb_height = 35;    
+    self.pageControl.zb_X = 0;
+    // pageControl的Y值 = ZBEmotionListView的高度 - pageControl的高度
+    // ZBEmotionListView的高度就是ZBEmotionKeyboard类中的layoutSubviews方法中的SaveShowingListView的高度，即self.SaveShowingListView.zb_height = self.zb_height - self.tabBar.zb_height;
+    self.pageControl.zb_Y = self.zb_height - self.pageControl.zb_height;
+    
+    // 2.scrollView
+    self.scrollView.zb_width = self.zb_width;
+    self.scrollView.zb_height = self.pageControl.zb_Y;
+    self.scrollView.zb_X = 0;
+    self.scrollView.zb_Y = 0;
+    
+    // 3.设置scrollView内部每一页的尺寸
+    NSUInteger count = self.scrollView.subviews.count;
+    for (int i = 0; i < count; i ++) {
+        // 把scrollView中存储的子控件，根据下标依次取出来，用pageView对象保存
+        UIView *pageView = self.scrollView.subviews[i];
+        pageView.zb_height = self.scrollView.zb_height;
+        pageView.zb_width = self.scrollView.zb_width;
+        // 每一页的X值 = 子控件的下标 * 每一页固定的宽度
+        pageView.zb_X = i * pageView.zb_width;
+        pageView.zb_Y = 0;
+    }
+    // 4.设置scrollView的ContentSize
+    // x轴的滚动范围是 计算出来的存放表情的UIView的个数 * 屏幕的宽度
+#warning 平白无故能多滚出一页，所以我这里将count减去了1，为什么能多滚出一页？现在还无法解释
+    self.scrollView.contentSize = CGSizeMake((count - 1) * self.scrollView.zb_width, 0);
+}
+
+#pragma mark - UIScrollViewDelegate
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    double  PageNumber = scrollView.contentOffset.x / scrollView.zb_width;
+    self.pageControl.currentPage = (int)(PageNumber + 0.5);
+}
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSLog(@"%@",self.scrollView.subviews);
+}
 
 @end
