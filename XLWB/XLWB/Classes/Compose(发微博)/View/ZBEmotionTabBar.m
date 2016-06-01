49c7ddbb973eb7147ee642dd1abc59495cdbe67f
zb <zb@163.com>
2016-06-01 12:30:06 +0800

    重写delegate的setter方法 ，解决点击键盘/表情图标时，没有显示默认对应的表情

--- XLWB/XLWB/Classes/Compose(发微博)/View/ZBEmotionTabBar.m
+++ XLWB/XLWB/Classes/Compose(发微博)/View/ZBEmotionTabBar.m
@@ -44,10 +44,10 @@
     [btn setTitle:title forState:UIControlStateNormal];
     // 将按钮添加到当前类中
     [self addSubview:btn];
-    // 打开表情键盘，默认就选中"默认"按钮
-    if (buttonType == ZBEmotionTabBarButtonTypeDefault) {
-        [self btnClick1:btn];
-    }
+//    // 打开表情键盘，默认就选中"默认"按钮
+//    if (buttonType == ZBEmotionTabBarButtonTypeDefault) {
+//        [self btnClick1:btn];
+//    }
     
     // 设置键盘底部按钮的背景图片
     if(self.subviews.count == 1){// 因为键盘底部按钮是一个一个加到当前类中的，所以当 当前类的数组中元素为1个时，就表示当前方法是添加第一个按钮的，所以满足self.subviews.count == 1,就将图片显示在键盘底部的第一个位置
@@ -78,6 +78,26 @@
     }
     
 }
+// 重写delegate的setter方法 ，解决点击键盘/表情图标时，没有显示"默认"对应的表情
+
+/* 
+ 原理:
+ 1.ZBEmotionKeyboard类成为ZBEmotionTabBar的代理，即tabBar.delegate = self;
+ 2.就会调用delegate的setter方法，在setter方法里面，让实参view绑定一个名为ZBEmotionTabBarButtonTypeDefault的tag，调用btnClick1:方法时，把view传递给形参
+ 3.在btnClick1:中，self.delegate此时有值，因为重写了setDelegate就是赋值操作，即_delegate = delegate。
+ 4.如果不重写setDelegate:方法，而将调用btnClick1:方法的代码写在上面的setUpBtn:buttonType:方法中时,self.delegate没有值，为空，所以就造成了点击键盘/表情图标时，没有显示"默认"对应的表情
+ */
+ -(void)setDelegate:(id<ZBEmotionTabBarDelegate>)delegate{
+    
+    _delegate = delegate;
+    // 强转成ZBEmotionTabBarButton类型
+    ZBEmotionTabBarButton *view = (ZBEmotionTabBarButton *)[self viewWithTag:ZBEmotionTabBarButtonTypeDefault];
+    
+    // 选中"默认"按钮
+    [self btnClick1:view];
+}
+
+
 /**
  *  按钮点击
  */
